ea56b32c4013b7b71f79a0903c9266a6d065a9b6
Sampa Rani Roy <samparaniroy99@gmail.com>
2022-05-03 21:23:53 +0600

    update

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+    }
+    const navigateRegister = event =>{
+        navigate('/register')
+    }
     
     return (
         <div className='login-form-area'>
             <h1>Please Login</h1>
-            <form>
-                <input type="text" name="email" placeholder='email' required/>
+            <form onSubmit={handleSubmit}>
+                <input ref={emailRef} type="text" name="email" placeholder='email' required/>
                 <br/>
                 <input type="password" name="password" id="" placeholder='password' required/>
                 <br/>
-                <input className='submit-button' type="submit" value="login" />
+                <input ref={passwordRef} className='submit-button' type="submit" value="login" />
             </form>
-            <p>If You Haven't Account Yet?</p>
+            <p>If You Haven't Account Yet? <Link to='/register' className='' onClick={navigateRegister}>Please Register</Link></p>
         </div>
     );
 };
